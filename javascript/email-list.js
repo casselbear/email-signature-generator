620b57630381674d6f708e7855be948359ac713f
casselbear <create@casselbear.com>
2025-07-14 10:23:47 -0400

    Updated email-list.js with new employees

--- javascript/email-list.js
+++ javascript/email-list.js
@@ -5,13 +5,15 @@
       "Derek Stulpin",
       "Frank Whanger",
       "Hannah Miller",
+      "Jake Muller",
       "Karl Bear",
       "Keegan Bear",
       "Nathan Van Twisk",
       "Ryan Howell",
       "Seth Teter",
       "Shaye Ladd",
-      "Zach Humm"
+      "Zach Humm",
+      "Zechariah Eshack"
   ];
 
   var employeeEmails = [
@@ -20,11 +22,13 @@
       "derek@casselbear.com",
       "frank@casselbear.com",
       "hannah@casselbear.com",
+      "jake@casselbear",
       "karl@casselbear.com",
       "keegan@casselbear.com",
       "nathan@casselbear.com",
       "ryanh@casselbear.com",
       "seth@casselbear.com",
       "shaye@casselbear.com",
-      "zach@casselbear.com"
+      "zach@casselbear.com",
+      "zechariah@casselbear.com"
     ];
